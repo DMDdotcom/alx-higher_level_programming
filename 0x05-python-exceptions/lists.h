168193f31de6b4018a4be77536dcfe55851f0975
DMDdotcom <diamond4mi@yahoo.com>
2023-03-29 21:01:34 -0700

    Python Classes and Objects

---
+++ 0x05-python-exceptions/lists.h
@@ -0,0 +1,13 @@
+Prototype: def safe_print_list(my_list=[], x=0):
+Prototype: def safe_print_integer(value):
+Prototype: def safe_print_list_integers(my_list=[], x=0):
+Prototype: def safe_print_division(a, b):
+Prototype: def list_division(my_list_1, my_list_2, list_length):
+Prototype: def raise_exception():
+Prototype: def raise_exception_msg(message=""):
+Prototype: def safe_print_integer_err(value):
+Prototype: def safe_function(fct, *args):
+Prototype: void print_python_list(PyObject *p);
+Prototype: void print_python_bytes(PyObject *p);
+Prototype: void print_python_bytes(PyObject *p);
+
